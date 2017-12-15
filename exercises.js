1287f16a9aea5b0d90683d3ea1c6c68d1cd67374
jblau07 <jlau7248@gmail.com>
2017-12-15 12:02:23 -1000

    completed exercise 11

--- exercises.js
+++ exercises.js
@@ -153,7 +153,10 @@ var createZeroFilledArray = function(num){
  * @param {Array}
  * @return {Array}
  */
-var poppedArray;
+var poppedArray = function(arr){
+  arr.pop();
+  return arr
+}
 
 /* #splitString
  *
@@ -440,7 +443,7 @@ module.exports = {
   convertToArray: convertToArray,
   objectSize: objectSize,
   createZeroFilledArray: createZeroFilledArray,
-  poppedArray: null,
+  poppedArray: poppedArray,
   splitString: null,
   lengthOfLast: null,
   sumBelowTen: null,
