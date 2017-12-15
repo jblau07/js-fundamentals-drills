8ade393bfa84f1805d901125ba0bed988e7578fd
jblau07 <jlau7248@gmail.com>
2017-12-15 11:29:10 -1000

    completed exercise 5

--- exercises.js
+++ exercises.js
@@ -66,7 +66,13 @@ var arrayLength = function(arr){
  * @param {Array}
  * @return {Number}
  */
-var countAll;
+var countAll = function(arr){
+  var result = 0;
+  for (var i = 0; i<arr.length; i++){
+    result += arr[i]
+  }
+  return result
+}
 
 /* #countStrings
  *
@@ -401,7 +407,7 @@ module.exports = {
   sumArrays: sumArrays,
   stringCount: stringCount,
   arrayLength: arrayLength,
-  countAll: null,
+  countAll: countAll,
   countStrings: null,
   countAllStrings: null,
   convertToArray: null,
